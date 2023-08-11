26c5efe9925052dadaf9cf94c778dcfba7cac880
Wind <>
2023-08-11 11:07:10 +0800

    Wind：首页卡片

--- lib/app/modules/edit_card/controllers/home_edit_card_controller.dart
+++ lib/app/modules/edit_card/controllers/home_edit_card_controller.dart
@@ -1,6 +1,7 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 import 'package:focusring/app/data/card_health_index.dart';
+import 'package:focusring/app/modules/home_state/controllers/home_state_controller.dart';
 import 'package:get/get.dart';
 
 import '../../../../public.dart';
@@ -8,59 +9,8 @@ import '../../../../public.dart';
 class HomeEditCardController extends GetxController {
   //TODO: Implement HomeEditCardController
 
-  RxList<DragAndDropList> datas = RxList.empty();
-
-  List<KHealthIndexModel> _visibleItems = [];
-  List<KHealthIndexModel> _hiddenItems = [];
-
-  Widget _getItemCard(KHealthDataType type) {
-    return Container(
-      height: 54.w,
-      key: ValueKey(type),
-      margin: EdgeInsets.only(left: 12.w, right: 12.w, bottom: 1),
-      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.w),
-      decoration: BoxDecoration(
-        color: ColorUtils.fromHex("#FF000000"),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              LoadAssetsImage(
-                type.getIcon(isCardIcon: true),
-                width: 35,
-                height: 35,
-              ),
-              8.rowWidget,
-              Text(
-                type.getDisplayName(isReport: true),
-                style: Get.textTheme.labelLarge,
-              ),
-            ],
-          ),
-          LoadAssetsImage(
-            "icons/editcard_icon_order",
-            width: 20,
-            height: 15,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _getItemTitle(String title) {
-    return Container(
-      key: ValueKey(title),
-      alignment: Alignment.centerLeft,
-      margin: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.w),
-      child: Text(
-        title,
-        style: Get.textTheme.labelMedium,
-      ),
-    );
-  }
+  RxList<KHealthIndexModel> visibleItems = <KHealthIndexModel>[].obs;
+  RxList<KHealthIndexModel> hiddenItems = <KHealthIndexModel>[].obs;
 
   @override
   void onInit() {
@@ -71,56 +21,10 @@ class HomeEditCardController extends GetxController {
 
   void _initData() async {
     final appUserId = await SPManager.getPhoneID();
-    _visibleItems = await KHealthIndexModel.queryAllWithState(appUserId, true);
-    _hiddenItems = await KHealthIndexModel.queryAllWithState(appUserId, false);
-    _updateData();
-  }
-
-  void _updateData() {
-    datas.value = [
-      DragAndDropList(
-        header: _getItemTitle("edit_cardtip".tr),
-        children: _visibleItems
-            .map((e) => DragAndDropItem(child: _getItemCard(e.type)))
-            .toList(),
-        canDrag: false,
-      ),
-      DragAndDropList(
-          canDrag: false,
-          header: _getItemTitle("hidden_Items".tr),
-          contentsWhenEmpty: DottedBorder(
-            color: ColorUtils.fromHex("#FF3C454A"),
-            strokeWidth: 1,
-            radius: Radius.circular(60),
-            child: Container(
-              height: 81.w,
-              width: 351.w,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(60),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  LoadAssetsImage(
-                    "icons/editcard_icon_hide",
-                    width: 39,
-                    height: 37,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10.w),
-                    child: Text(
-                      "hidden_Itemsdesc".tr,
-                      style: Get.textTheme.labelMedium,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          children: _hiddenItems
-              .map((e) => DragAndDropItem(child: _getItemCard(e.type)))
-              .toList())
-    ];
+    visibleItems.value =
+        await KHealthIndexModel.queryAllWithState(appUserId, true);
+    hiddenItems.value =
+        await KHealthIndexModel.queryAllWithState(appUserId, false);
     update();
   }
 
@@ -136,51 +40,48 @@ class HomeEditCardController extends GetxController {
 
   void onItemReorder(
       int oldItemIndex, int oldListIndex, int newItemIndex, int newListIndex) {
-    // var movedItem = datas[oldListIndex].children.removeAt(oldItemIndex);
-    // datas[newListIndex].children.insert(newItemIndex, movedItem);
-    // update();
-    vmPrint(
-        "oldItemIndex $oldItemIndex  $oldListIndex  $newItemIndex $newListIndex ");
-
     KHealthIndexModel draggedItem;
+    try {
+      // 从旧列表中提取被拖动的项
+      if (oldListIndex == 0) {
+        // 显示列表
+        draggedItem = visibleItems[oldItemIndex];
+        visibleItems.removeAt(oldItemIndex);
+      } else {
+        // 隐藏列表
+        draggedItem = hiddenItems[oldItemIndex];
+        hiddenItems.removeAt(oldItemIndex);
+      }
+      // // 插入到新的位置
+      if (newListIndex == 0) {
+        // 显示列表
+        visibleItems.insert(newItemIndex, draggedItem);
+        draggedItem.state = true; // 设置为可见
+      } else {
+        // 隐藏列表
+        hiddenItems.insert(newItemIndex, draggedItem);
+        draggedItem.state = false; // 设置为隐藏
+      }
 
-    // 从旧列表中提取被拖动的项
-    if (oldListIndex == 0) {
-      // 显示列表
-      draggedItem = _visibleItems[oldItemIndex];
-      _visibleItems.removeAt(oldItemIndex);
-    } else {
-      // 隐藏列表
-      draggedItem = _hiddenItems[oldItemIndex];
-      _hiddenItems.removeAt(oldItemIndex);
+      // 更新index字段
+      for (int i = 0; i < visibleItems.length; i++) {
+        visibleItems[i].index = i;
+      }
+      for (int i = 0; i < hiddenItems.length; i++) {
+        hiddenItems[i].index = i;
+      }
+
+      KHealthIndexModel.updateTokens(visibleItems);
+      KHealthIndexModel.updateTokens(hiddenItems);
+      update();
+     final c  = Get.find<HomeStateController>();
+     c.initData();
+    } catch (e) {
+      vmPrint(e.toString());
     }
-
-    // 插入到新的位置
-    if (newListIndex == 0) {
-      // 显示列表
-      _visibleItems.insert(newItemIndex, draggedItem);
-      draggedItem.state = true; // 设置为可见
-    } else {
-      // 隐藏列表
-      _hiddenItems.insert(newItemIndex, draggedItem);
-      draggedItem.state = false; // 设置为隐藏
-    }
-
-    // // 更新index字段
-    // for (int i = 0; i < _visibleItems.length; i++) {
-    //   _visibleItems[i].index = i;
-    // }
-    // for (int i = 0; i < _hiddenItems.length; i++) {
-    //   _hiddenItems[i].index = i;
-    // }
-
-    update();
   }
 
   void onListReorder(int oldListIndex, int newListIndex) {
     vmPrint("onListReorder $oldListIndex   $newListIndex ");
-    var movedList = datas.removeAt(oldListIndex);
-    datas.insert(newListIndex, movedList);
-    update();
   }
 }
--- lib/app/modules/edit_card/views/home_edit_card_view.dart
+++ lib/app/modules/edit_card/views/home_edit_card_view.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:focusring/utils/console_logger.dart';
 
@@ -10,17 +11,110 @@ import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 class HomeEditCardView extends GetView<HomeEditCardController> {
   const HomeEditCardView({Key? key}) : super(key: key);
 
+  Widget _getItemCard(KHealthDataType type) {
+    return Container(
+      height: 54.w,
+      key: ValueKey(type),
+      margin: EdgeInsets.only(left: 12.w, right: 12.w, bottom: 1),
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.w),
+      decoration: BoxDecoration(
+        color: ColorUtils.fromHex("#FF000000"),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              LoadAssetsImage(
+                type.getIcon(isCardIcon: true),
+                width: 35,
+                height: 35,
+              ),
+              8.rowWidget,
+              Text(
+                type.getDisplayName(isReport: true),
+                style: Get.textTheme.labelLarge,
+              ),
+            ],
+          ),
+          LoadAssetsImage(
+            "icons/editcard_icon_order",
+            width: 20,
+            height: 15,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getItemTitle(String title) {
+    return Container(
+      key: ValueKey(title),
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.w),
+      child: Text(
+        title,
+        style: Get.textTheme.labelMedium,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return KBasePageView(
       titleStr: "edit_card".tr,
       body: GetBuilder(
         init: controller,
-        builder: ((_) => DragAndDropLists(
-              children: controller.datas,
+        builder: ((a) => DragAndDropLists(
+              children: [
+                DragAndDropList(
+                  header: _getItemTitle("edit_cardtip".tr),
+                  children: a.visibleItems
+                      .map((e) => DragAndDropItem(child: _getItemCard(e.type)))
+                      .toList(),
+                  canDrag: false,
+                ),
+                DragAndDropList(
+                    canDrag: false,
+                    header: _getItemTitle("hidden_Items".tr),
+                    contentsWhenEmpty: DottedBorder(
+                      color: ColorUtils.fromHex("#FF3C454A"),
+                      strokeWidth: 1,
+                      radius: Radius.circular(60),
+                      child: Container(
+                        height: 81.w,
+                        width: 351.w,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(60),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            LoadAssetsImage(
+                              "icons/editcard_icon_hide",
+                              width: 39,
+                              height: 37,
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 10.w),
+                              child: Text(
+                                "hidden_Itemsdesc".tr,
+                                style: Get.textTheme.labelMedium,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    children: a.hiddenItems
+                        .map(
+                            (e) => DragAndDropItem(child: _getItemCard(e.type)))
+                        .toList())
+              ],
               removeTopPadding: true,
               listDragOnLongPress: false,
-              lastItemTargetHeight: 10,
+              // lastItemTargetHeight: 10,
               onItemReorder: controller.onItemReorder,
               onListReorder: controller.onListReorder,
             )),
