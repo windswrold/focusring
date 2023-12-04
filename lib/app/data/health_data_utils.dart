a76a944e4e7b1f9938cfeb531980e75c5ca27507
Wind <>
2023-12-04 15:49:54 +0800

    Wind：修改

--- lib/app/data/health_data_utils.dart
+++ lib/app/data/health_data_utils.dart
@@ -18,64 +18,7 @@ import '../../views/charts/home_card/model/home_card_x.dart';
 import 'health_data_model.dart';
 
 class HealthDataUtils {
-  List<BloodOxygenData>? bloodOxygenData;
-  List<FemalePeriodData>? femalePeriodData;
-  List<HeartRateData>? heartRateData;
-  List<SleepData>? sleepData;
-  List<TempData>? tempData;
-  List<StepData>? stepData;
-  List<EmotionData>? emotionData;
-  List<PressureData>? pressureData;
-
-  HealthDataUtils({
-    this.bloodOxygenData,
-    this.femalePeriodData,
-    this.heartRateData,
-    this.sleepData,
-    this.tempData,
-    this.stepData,
-    this.emotionData,
-    this.pressureData,
-  });
-
-  factory HealthDataUtils.fromJson(Map<String, dynamic> json) =>
-      HealthDataUtils(
-        bloodOxygenData: (json["bloodOxygenData"] as List)
-            .map((i) => BloodOxygenData.fromJson(i))
-            .toList(),
-        femalePeriodData: (json["femalePeriodData"] as List)
-            .map((i) => FemalePeriodData.fromJson(i))
-            .toList(),
-        heartRateData: (json["heartRateData"] as List)
-            .map((i) => HeartRateData.fromJson(i))
-            .toList(),
-        sleepData: (json["sleepData"] as List)
-            .map((i) => SleepData.fromJson(i))
-            .toList(),
-        tempData: (json["tempData"] as List)
-            .map((i) => TempData.fromJson(i))
-            .toList(),
-        stepData: (json["stepData"] as List)
-            .map((i) => StepData.fromJson(i))
-            .toList(),
-        emotionData: (json["emotionData"] as List)
-            .map((i) => EmotionData.fromJson(i))
-            .toList(),
-        pressureData: (json["pressureData"] as List)
-            .map((i) => PressureData.fromJson(i))
-            .toList(),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "bloodOxygenData": bloodOxygenData?.map((x) => x.toJson()).toList(),
-        "femalePeriodData": femalePeriodData?.map((x) => x.toJson()).toList(),
-        "heartRateData": heartRateData?.map((x) => x.toJson()).toList(),
-        "sleepData": sleepData?.map((x) => x.toJson()).toList(),
-        "tempData": tempData?.map((x) => x.toJson()).toList(),
-        "stepData": stepData?.map((x) => x.toJson()).toList(),
-        "emotionData": emotionData?.map((x) => x.toJson()).toList(),
-        "pressureData": pressureData?.map((x) => x.toJson()).toList(),
-      };
+  HealthDataUtils();
 
   static void queryHealthData({
     required KHealthDataType types,
--- lib/const/event_bus_class.dart
+++ lib/const/event_bus_class.dart
@@ -1,11 +1,14 @@
+import 'constant.dart';
+
 class KReportQueryTimeUpdate {
   final DateTime time;
 
   KReportQueryTimeUpdate(this.time);
 }
 
-
 //数据完成
 class KReportQueryDataUpdate {
-  KReportQueryDataUpdate();
+  KHealthDataType? refreType;
+
+  KReportQueryDataUpdate({this.refreType});
 }
